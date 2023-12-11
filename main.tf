fb7d7c17e0b175b8aaaa8e044a8313b46bb06382
Gopika <gops@gmail.com>
2023-12-11 15:17:45 +0000

    Resource2 Security Group

--- main.tf
+++ main.tf
@@ -10,3 +10,51 @@ resource "aws_key_pair" "git_key" {
     owner   = var.project_own
   }
 }
+
+##SECURITY GROUP ##
+
+resource "aws_security_group" "http_access" {
+  name        = "http-sg-${var.project_name}-${var.project_env}"
+  description = "${var.project_name}-${var.project_env}"
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name    = "sg-http-${var.project_name}-${var.project_env}"
+    project = var.project_name
+    env     = var.project_env
+    owner   = var.project_own
+  }
+}
