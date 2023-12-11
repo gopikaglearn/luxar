f95b022b0f50c0651b6b5b9e838d9297300c52ea
Gopika <gops@gmail.com>
2023-12-11 15:29:23 +0000

    Resource 3 Remote access SG

--- main.tf
+++ main.tf
@@ -58,3 +58,33 @@ resource "aws_security_group" "http_access" {
     owner   = var.project_own
   }
 }
+###SECURITY GROUP REMOTE ACCESS ##
+
+resource "aws_security_group" "ssh_access" {
+  name        = "ssh-${var.project_name}-${var.project_env}"
+  description = "${var.project_name}-${var.project_env}"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name    = "${var.project_name}-${var.project_env}-ssh-access"
+    project = var.project_name
+    env     = var.project_env
+    owner   = var.project_own
+  }
+}
